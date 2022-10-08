f5c0f836fcc0fa8a46ceff4f3af0c4f10a505a07
MIhirchavda <mihirchavda1362@gmail.com>
2022-10-08 23:42:28 +0530

    encapsulation code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        //r.name = "mihir";
+        //java: name has private access in com.driver.RWOnly
+        r.setName("mihir");
+        System.out.println(r.getName());
+    }
 }
